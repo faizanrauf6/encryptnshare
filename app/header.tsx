06a715f5f4627ab4ca0d8fb1d2bae18dbf118ec5
Muhammad Faizan Rauf <jasper@mufin.co.kr>
2025-06-13 12:27:15 +0500

    Commented out theme functionality

--- app/header.tsx
+++ app/header.tsx
@@ -40,14 +40,14 @@ export const Header: React.FC = () => {
                   </Link>
                 </li>
               ))}
-              <li>
+              {/* <li>
                 <button
                   onClick={toggleTheme}
                   className="text-sm sm:text-base px-3 py-2 transition-colors text-zinc-100 hover:text-zinc-50"
                 >
                   {theme === 'dark' ? '☀️' : '🌙'}
                 </button>
-              </li>
+              </li> */}
             </ul>
           </nav>
         </div>
